b77dbbda4a1719e203828459a2c109b4b90abcaa
abolfazl2643 <abolfazlrezaei.v@gmail.com>
2021-10-09 18:11:22 +0330

    add advertisment entity

---
+++ lib/features/advertisement/domain/entities/advertisement.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class Advertisement extends Equatable {
+  final String title;
+  final int price;
+  final DateTime datePosted;
+  final String description;
+  final Image photo;
+
+  Advertisement({
+    required this.photo,
+    required this.price,
+    required this.datePosted,
+    required this.description,
+    required this.title,
+  });
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
