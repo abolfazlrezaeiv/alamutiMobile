7d89585b690fdf643ef2ee0ccf4091270e0be764
abolfazl2643 <abolfazlrezaei.v@gmail.com>
2021-11-09 22:59:42 +0330

    photo add ui

--- lib/foodad.dart
+++ lib/foodad.dart
@@ -1,4 +1,5 @@
 import 'package:alamuti/bottom_navbar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FoodAdForm extends StatelessWidget {
@@ -7,25 +8,59 @@ class FoodAdForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ثبت آگهی'),
-          backgroundColor: Color.fromRGBO(8, 212, 76, 0.5),
-          automaticallyImplyLeading: false,
+      appBar: AppBar(
+        title: Text('ثبت آگهی'),
+        backgroundColor: Color.fromRGBO(8, 212, 76, 0.5),
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: AlamutBottomNavBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.width / 10),
+                      child: Icon(CupertinoIcons.photo_camera),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.width / 10),
+                      child: Icon(CupertinoIcons.photo_camera),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.width / 10),
+                      child: Icon(CupertinoIcons.photo_camera),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Container(
+                height: 100,
+                child: Text(
+                  'عنوان',
+                  style: TextStyle(fontSize: 25),
+                  textDirection: TextDirection.rtl,
+                ),
+              ),
+            )
+          ],
         ),
-        bottomNavigationBar: AlamutBottomNavBar(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20),
-          child: GridView.count(
-            children: [
-              Card(),
-              Card(),
-              Card(),
-              Card(),
-              Card(),
-              Card(),
-            ],
-            crossAxisCount: 3,
-          ),
-        ));
+      ),
+    );
   }
 }
