8d2fe41341a25ec06e58c56250a9bc494e98140c
abolfazl.rezaei <abolfazlrezaei.v@gmail.com>
2022-12-25 17:12:15 +0330

    add bloc state management

---
+++ lib/app/data/entities/auth_byphone_response.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_byphone_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AuthenticationByPhoneResponse _$$_AuthenticationByPhoneResponseFromJson(
+        Map<String, dynamic> json) =>
+    _$_AuthenticationByPhoneResponse(
+      phonenumber: json['phonenumber'] as String?,
+      userId: json['userId'] as String?,
+    );
+
+Map<String, dynamic> _$$_AuthenticationByPhoneResponseToJson(
+        _$_AuthenticationByPhoneResponse instance) =>
+    <String, dynamic>{
+      'phonenumber': instance.phonenumber,
+      'userId': instance.userId,
+    };
